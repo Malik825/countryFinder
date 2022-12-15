9fe19c064ac2f1cb9664246e59dcb1fad39cb2c0
Sule Malik <abdulmaliksuleman75@gmail.com>
2022-12-15 07:19:27 -1200

    second Commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 let searchBtn = document.getElementById('search-btn');
 let countryInput = document.getElementById('country-inp');
 
-searchBtn.addEventListener('click', ()=>{
-    let countryName = countryInput.value;
+let closeResults = ()=>{
+    document.querySelector('#results').classList.add('active');
+
+}
+countryInput.addEventListener('click', closeResults)
+searchBtn.addEventListener('click', async()=>{
+  document.querySelector('#results').classList.remove('active');
+    
+    let countryName = await countryInput.value;
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+
     fetch(finalURL).then((response)=>response.json())
     .then((data)=>{
-        console.log(data[0]);
-        console.log(data[0].capital[0]);
-        console.log(data[0].flags.svg);
-        console.log(data[0].name.common);
-        console.log(data[0].continents[0]);
-        console.log(Object.keys(data[0].currencies)[0]);
-    console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-    console.log(Object.values(data[0].languages).toString().split(",").join(", "));
     results.innerHTML = `
     <img src ="${data[0].flags.svg}" class="flag-img" />
     <h2>${data[0].name.common}</h2>
